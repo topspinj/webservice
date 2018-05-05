60be531d72aba3b5045446e402ca64080a982823
Jill Cates <cates.jill@gmail.com>
2018-05-05 15:46:17 -0700

    Set up gh webhook

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -8,8 +8,14 @@ router = routing.Router()
 
 @router.register("issues", action="opened")
 async def issue_opened_event(event, gh, *args, **kwargs):
-    """ Whenever an issue is opened, greet the author and say thanks."""
-    pass
+    """
+    Whenever an issue is opened, greet the author and say thanks.
+    """
+    url = event.data["issue"]["comments_url"]
+    author = event.data["issue"]["user"]["login"]
+
+    message = f"Thanks for the report @{author}! I will look into it ASAP! (I'm a bot)."
+    await gh.post(url, data={"body": message})
 
 async def main(request):
     # read the GitHub webhook payload
